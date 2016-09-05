47fa2293716cf594ea7d816b4a3c8f6946255b16
JunhoChoi <ccg320@toss.im>
2016-09-05 11:31:28 +0900

    https://velopert.com/900

--- 2.React_first/src/App.js
+++ 2.React_first/src/App.js
@@ -1,27 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Header/>
+        <Content/>
       </div>
     );
   }
 }
 
 export default App;
-//
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('example')
-// );
---
+++ 2.React_first/src/components/Content.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Content extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Content</h2>
+        <p>Hey!</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
