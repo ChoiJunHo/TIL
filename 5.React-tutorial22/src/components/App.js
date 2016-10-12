ff288f7ec48d9cad53398c785b21d9d8a4238637
JunhoChoi <ccg320@toss.im>
2016-10-12 11:34:44 +0900

    ㅁㅁ

--- 5.React-tutorial22/src/components/App.js
+++ 5.React-tutorial22/src/components/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import RandomNumber from './RandomNumber';
 
 class App extends React.Component {
     render(){
-
         return (
-                <h1>Hello React Skeleton</h1>
+          <div>
+            <h1>{this.state.header}</h1>
+            <h1>{this.state.content}</h1>
+            <Header   title={this.props.headerTitle}/>
+            <Content  title={this.props.contentTitle} body={this.props.contentBody}/>
+            <button onClick={this._updateHeader.bind(this)}>Update</button>
+            <RandomNumber number={this.state.randomNumber} onUpdate={this._updateRandomNumber}/>
+          </div>
         );
     }
+
+    constructor(props){
+      super(props);
+      this.state = {
+        header: "Header State",
+        content: "Content State",
+        randomNumber: Math.round(Math.random() * 100)
+      };
+      this._updateRandomNumber = this._updateRandomNumber.bind(this);
+    }
+
+    _updateRandomNumber(value){
+      this.setState({
+        randomNumber: value
+      });
+    }
+
+    _updateHeader(text){
+      this.setState({
+        header: "Header has changed"
+      });
+    }
 }
+App.defaultProps = {
+  headerTitle: "Default headerTitle",
+  contentTitle: "5",
+  contentBody: "Default contentBody"
+};
 
 export default App;
